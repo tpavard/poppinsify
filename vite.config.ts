f1c9843e12220fccf1f1fdd66aecd0baa4abe9b0
tpavard <22837800+tpavard@users.noreply.github.com>
2025-02-17 04:46:42 +0100

    🔧 Update formatter and typescript configurations

--- vite.config.ts
+++ vite.config.ts
@@ -7,33 +7,40 @@ import {
 	URL,
 } from "node:url";
 
-import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+
+import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		// dts({ tsconfigPath: "./tsconfig.app.json" }),
-		vue(),
-		vueDevTools(),
-	],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./lib", import.meta.url)),
+export default defineConfig(({ command, isPreview }) => {
+	return {
+		plugins: isPreview === true || command === "serve"
+			? [
+				vue(),
+				vueDevTools(),
+			]
+			: [
+				dts({ tsconfigPath: "./tsconfig.app.json" }),
+				vue(),
+			],
+		resolve: {
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+			},
 		},
-	},
-	build: {
-		lib: {
-			entry: resolve(__dirname, "lib/main.ts"),
-			formats: ["es"],
-			fileName: "main",
+		build: {
+			lib: {
+				entry: resolve(__dirname, "lib/main.ts"),
+				formats: ["es"],
+				fileName: "main",
+			},
+			rollupOptions: {
+				external: ["vue"],
+			},
 		},
-		rollupOptions: {
-			external: ["vue"],
-		},
-	},
+	};
 });
